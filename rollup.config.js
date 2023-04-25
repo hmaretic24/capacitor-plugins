BastienRodz/capacitor-local-notifications

feat: add commonjs output format (#179)

rollup.config.js
@@ -1,14 +1,22 @@
 export default {
   input: 'dist/esm/index.js',
-  output: {
-    file: 'dist/plugin.js',
-    format: 'iife',
-    name: 'capacitorLocalNotifications',
-    globals: {
-      '@capacitor/core': 'capacitorExports',
+  output: [
+    {
+      file: 'dist/plugin.js',
+      format: 'iife',
+      name: 'capacitorHaptics',
+      globals: {
+        '@capacitor/core': 'capacitorLocalNotifications',
+      },
+      sourcemap: true,
+      inlineDynamicImports: true,
     },
-    sourcemap: true,
-    inlineDynamicImports: true,
-  },
+    {
+      file: 'dist/plugin.cjs.js',
+      format: 'cjs',
+      sourcemap: true,
+      inlineDynamicImports: true,
+    },
+  ],
   external: ['@capacitor/core'],
 };

hmaretic24/capacitor-plugins

rollup.config.js
@@ -1,14 +1,22 @@
 export default {
   input: 'dist/esm/index.js',
-  output: {
-    file: 'dist/plugin.js',
-    format: 'iife',
-    name: 'capacitorBrowser',
-    globals: {
-      '@capacitor/core': 'capacitorExports',
+  output: [
+    {
+      file: 'dist/plugin.js',
+      format: 'iife',
+      name: 'capacitorBrowser',
+      globals: {
+        '@capacitor/core': 'capacitorExports',
+      },
+      sourcemap: true,
+      inlineDynamicImports: true,
     },
-    sourcemap: true,
-    inlineDynamicImports: true,
-  },
+    {
+      file: 'dist/plugin.cjs.js',
+      format: 'cjs',
+      sourcemap: true,
+      inlineDynamicImports: true,
+    },
+  ],
   external: ['@capacitor/core'],
 };